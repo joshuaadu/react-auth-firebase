1f2b7e1c20db298e802b541a355222ddd57c1afc
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-03-25 23:35:23 +0000

    add use-auth hook to authenticate existing users and sign up new users

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,34 +1,59 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from "react";
+import useAuth from "../../hooks/use-auth";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const { isloading, data, error, requestAuth } = useAuth();
+
+  useEffect(() => {
+    console.log("isLoading: ", isloading);
+    console.log("data: ", data);
+    console.log("error: ", error);
+  }, [data, isloading, error]);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+    const userInfo = {
+      email: emailInputRef.current.value,
+      password: passwordInputRef.current.value
+    };
+    console.log(userInfo);
+    requestAuth(userInfo, isLogin);
+  };
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitFormHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
---
+++ src/hooks/use-auth.js
@@ -0,0 +1,41 @@
+import { useCallback, useState } from "react";
+
+const useAuth = () => {
+  const [isloading, setIsLoading] = useState(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const requestAuth = useCallback(async (userInfo, isLogin) => {
+    setIsLoading(true);
+    const url = isLogin
+      ? "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCEd7g2Ium7dFMYKYIqUD6XDCblMd7aCbk"
+      : "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCEd7g2Ium7dFMYKYIqUD6XDCblMd7aCbk";
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          email: userInfo.email,
+          password: userInfo.password,
+          returnSecureToken: true
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(`Authentication failed: ${data.error.message}`);
+      }
+      console.log(data);
+      if (data) {
+        setData(data);
+        setError(null);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+  return { isloading, data, error, requestAuth };
+};
+export default useAuth;

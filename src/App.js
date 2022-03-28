12f14decd955ae5a35cee1521e46cab2fb4e7bb6
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-03-28 11:30:13 +0000

    Protect frontend pages with conditional routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
 import UserProfile from "./components/Profile/UserProfile";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const AuthCtx = useContext(AuthContext);
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="auth" element={<AuthPage />} />
-        <Route path="profile" element={<UserProfile />} />
+        {!AuthCtx.isLoggedIn && <Route path="auth" element={<AuthPage />} />}
+        <Route
+          path="profile"
+          element={
+            AuthCtx.isLoggedIn ? <UserProfile /> : <Navigate to="/auth" />
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Layout>
   );

ba1cfe65eb2e3b559358b85830734b90575f72cc
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-03-28 15:48:55 +0000

    store/persist user authentication status using localStorage

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -10,15 +10,18 @@ const initialValue = {
 const AuthContext = createContext(initialValue);
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem("token", token);
   };
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
 
   const contextValue = {
